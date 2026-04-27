d698cb3c5665c18764caaf509ead39d6e20048a9
dystal-dev <jeremyhabal@yahoo.com>
2026-04-27 15:56:43 +0800

    refactor: extract generateShortCode to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function generateShortCode(length: number): string {
+  return Math.random()
+    .toString(36)
+    .slice(2, length + 2);
 }
