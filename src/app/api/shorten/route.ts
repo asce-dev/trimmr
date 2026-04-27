9987bd4ca1d97387a0e6200d3382d46e4e2ac416
dystal-dev <jeremyhabal@yahoo.com>
2026-04-27 15:56:54 +0800

    feat: add url format validation

--- src/app/api/shorten/route.ts
+++ src/app/api/shorten/route.ts
@@ -1,32 +1,37 @@
 import { createClient } from "@supabase/supabase-js";
-
-function generateString(length: number) {
-  const result = Math.random()
-    .toString(36)
-    .slice(2, length + 2);
-  return result;
-}
+import { generateShortCode } from "@/lib/utils";
 
 export async function POST(request: Request) {
   const body: { url: string } = await request.json();
   const url = body.url;
+
   if (!url) {
     return Response.json({ error: "Missing url" }, { status: 400 });
   }
-  const shortUrl = generateString(6);
+
+  try {
+    new URL(url);
+  } catch {
+    return Response.json({ error: "Invalid URL format" }, { status: 400 });
+  }
+
+  const shortUrl = generateShortCode(6);
 
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   );
+
   const { error } = await supabase.from("links").insert({
     original_url: url,
     short_url: shortUrl,
   });
-  console.log("insert error:", error);
 
   if (error) {
-    return Response.json({ error: "DB insert failed" }, { status: 500 });
+    return Response.json(
+      { error: "Failed to create short link" },
+      { status: 500 },
+    );
   }
 
   return Response.json({ originalUrl: url, shortUrl: shortUrl });
